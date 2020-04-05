36fbb2715d95dfc9ff3a54fb9ef821d31e81d5ce
Uros <40537592+urosbmx@users.noreply.github.com>
2020-04-05 22:17:09 +0200

    Updates

---
+++ precache-manifest.83499984df74749cba5008d180968b88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db36b78a16165674971442cef8861695",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "ef05a7f4e838a5580b1c",
+    "url": "/react-app/static/css/main.25e7a536.chunk.css"
+  },
+  {
+    "revision": "3b635b67f848001832be",
+    "url": "/react-app/static/js/2.9998726b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.9998726b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef05a7f4e838a5580b1c",
+    "url": "/react-app/static/js/main.d4de66a9.chunk.js"
+  },
+  {
+    "revision": "9eed5efbc3ef6a69ca17",
+    "url": "/react-app/static/js/runtime-main.581c8c0a.js"
+  }
+]);
